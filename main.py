4bc504ef04b2653d34847bc28428383051234a4d
anthonysea <anthonyqchao@gmail.com>
2017-03-07 23:03:03 -0800

    now displays images

--- main.py
+++ main.py
@@ -7,25 +7,44 @@ import numpy
 
 class GUI:
     def __init__(self, master):
-        self.master = master
-        master.title("Simple GUI")
+        frame = Frame(master)
+        frame.pack()
 
-        self.label = Label(master, text="This is our first GUI!")
+        self.label = Label(frame, text="This is our first GUI!")
         self.label.pack()
 
-        self.select_scene_btn = Button(master, text="Select scene", command=self.getImagePath)
+        self.select_scene_btn = Button(frame, text="Select scene", command=lambda: self.displayImage("scene"),
+                                       padx="5", pady="5", width="10")
         self.select_scene_btn.pack()
 
-        self.select_sprite_btn = Button(master, text="Select sprite", command=self.getImagePath)
+        self.select_sprite_btn = Button(frame, text="Select sprite", command=lambda: self.displayImage("sprite"),
+                                        padx="5", pady="5", width="10")
         self.select_sprite_btn.pack()
 
-        self.close_button = Button(master, text="Close", command=master.quit)
+        self.close_button = Button(frame, text="Close", command=frame.quit,
+                                   padx="5", pady="5", width="10")
         self.close_button.pack()
 
+        self.sceneCanvas = Canvas(root)
+        self.spriteCanvas = Canvas(root)
 
-    def getImagePath(self):
+
+    def displayImage(self, identifier):
         self.imgPath = filedialog.askopenfilename()
-        return self.imgPath if len(self.imgPath) > 0 else "No image selected"
+        if identifier == "scene":
+            self.scenePhoto = ImageTk.PhotoImage(Image.open(self.imgPath))
+            self.sceneCanvas.create_image((0, 0), image=self.scenePhoto, anchor="nw")
+            self.sceneCanvas.image = self.scenePhoto
+            self.sceneCanvas.pack()
+
+        else:
+            self.spritePhoto = ImageTk.PhotoImage(Image.open(self.imgPath))
+            self.spriteCanvas.create_image((0, 0), image=self.spritePhoto, anchor="nw")
+            self.spriteCanvas.image = self.spritePhoto
+            self.spriteCanvas.pack()
+
+        # return self.imgPath if len(self.imgPath) > 0 else "No image selected"
+
 
 root = Tk()
 my_gui = GUI(root)
